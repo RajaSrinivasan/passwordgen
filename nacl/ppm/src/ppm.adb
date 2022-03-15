f538c0a3e80f10069ea2e0b1ae43cf74f848a178
R Srinivasan <rajasrinivasan@hotmail.com>
2022-03-15 05:44:51 -0400

    add clic

--- nacl/ppm/src/ppm.adb
+++ nacl/ppm/src/ppm.adb
@@ -1,14 +1,18 @@
 with Ada.Text_Io; use Ada.Text_Io ;
 with Ada.Command_Line; use Ada.Command_Line ;
+
+with Clic.tty ;
+
 with words ;
 with words_str ;
 with numbers ;
 with passwords ;
 
 procedure ppm is
-   -- np : Integer := Integer'Value(Argument(1)) ;
+   np : Integer := Integer'Value(Argument(1)) ;
    cand : words.CandidateWords_Type ;
 begin
    cand := words.Initialize(words_str.words , words_str.SEPARATOR );
-   Put_Line(passwords.Generate (wordlist => cand , segs => 3 , sep => "$"));
+   clic.tty.Enable_Color ;
+   Put_Line(Clic.tty.Emph(passwords.Generate (wordlist => cand , segs => np , sep => "$")));
 end ppm ;
