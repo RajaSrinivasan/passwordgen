e20cfa75930fc00e8fedf1fd5168da0130731e07
R Srinivasan <rajasrinivasan@hotmail.com>
2022-03-17 04:52:36 -0400

    changed salt

--- nacl/ppm/src/kdf.adb
+++ nacl/ppm/src/kdf.adb
@@ -13,7 +13,7 @@ with numbers ;
 package body kdf is
 
   function DeriveKey( pwd : aliased string ;
-                       salt : aliased string := "A big bug bit the little beetle but the little beetle bit the big bug back";
+                      salt : aliased string := "pangram: the five boxing wizards jump quickly" ;
                       iterations : integer := 2 )
                      return String is
 
--- nacl/ppm/src/kdf.ads
+++ nacl/ppm/src/kdf.ads
@@ -2,7 +2,7 @@
 package kdf is
    
    function DeriveKey( pwd : aliased string ;
-                       salt : aliased string := "A big bug bit the little beetle but the little beetle bit the big bug back";
+                       salt : aliased string := "pangram: the five boxing wizards jump quickly" ;
                        iterations : integer := 2 )
      return String ;
 
