c491d27eb736da1bc1878a4b6bdaf999a819362a
R Srinivasan <rajasrinivasan@hotmail.com>
2022-03-25 06:30:39 -0400

    more words

--- src/words_str.ads
+++ src/words_str.ads
@@ -201,5 +201,6 @@ package words_str is
 	 "ovine,bovine,porcine," &
 	 "embryo,spasm,aspirate,pulsatile,arterial,venous," &
 	 "newton,einstein,galileo,gauss,fourier,harvey,maxwell,faraday,boole,higgs,chandra,bose,fermat,volta," &
+	 "pangram,kennings,antigram,ambigram,acrostics," & 
      "" ; 
 end words_str ;
